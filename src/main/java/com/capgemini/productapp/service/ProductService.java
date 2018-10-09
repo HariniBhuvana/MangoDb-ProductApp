47ff9b15dbc6c9e24fed1296ac9e490d1b54de99
Harink <Harink@IN-MUM-LDVM039.corp.capgemini.com>
2018-10-09 16:12:46 +0530

    Mongodb-ProductApp

---
+++ src/main/java/com/capgemini/productapp/service/ProductService.java
@@ -0,0 +1,26 @@
+package com.capgemini.productapp.service;
+
+import java.util.List;
+
+import com.capgemini.productapp.entities.Product;
+import com.capgemini.productapp.exeception.ProductNotFoundException;
+
+public interface ProductService {
+
+	public Product addProduct(Product product);
+
+	public Product updateProduct(Product product);
+
+	public Product findProductById(int productId) throws ProductNotFoundException;
+
+	public void deleteProduct(Product product);
+
+	public List<Product> findProductByName(String productName) throws ProductNotFoundException;
+
+	public List<Product> findProductByCategory(String productCategory) throws ProductNotFoundException;
+
+	public List<Product> findProductByCategoryAndPrice(String productCategory, double maxprice, double minprice);
+
+	public List<Product> findProductByPrice(double productPrice) throws ProductNotFoundException;
+
+}
